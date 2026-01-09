efd40dbb73f47572357f37afa536489aad63ff2f
caizhihao <caizhihao@iyinguo.com>
2026-01-09 19:07:09 +0800

    feat: chat

--- app/api/[[...route]]/chat.ts
+++ app/api/[[...route]]/chat.ts
@@ -4,14 +4,17 @@ import z, { string } from "zod"
 import { getAuthUser } from "@/lib/hono/hono-middlware"
 import prisma from "@/lib/prisma"
 import { HTTPException } from "hono/http-exception"
-import type { UIMessage } from "ai"
-import type { ChatModel } from "@/lib/ai/models"
+import { convertToModelMessages, stepCountIs, streamText, type UIMessage, type UIMessagePart } from "ai"
+import { DEVELOPMENT_CHAT_MODEL, type ChatModel } from "@/lib/ai/models"
+import { generateTitleForUserMessage } from "@/lib/actions"
+import { isProduction, ModelProvider } from "@/lib/ai/providers"
+import { generateUUID } from "@/lib/utils"
 
 const chatSchema = z.object({
   id: z.string().min(1),
   message: z.custom<UIMessage>(),
   // selectedModel: z.custom<ChatModel["id"]>(),
-  selectedModel: z.string() as z.ZodType<ChatModel["id"]>,
+  selectedModelId: z.string() as z.ZodType<ChatModel["id"]>,
   selectedToolName: z.string().nullable(),
 })
 
@@ -19,4 +22,98 @@ const chatIdSchema = z.object({
   id: z.string().min(1),
 })
 
-export const chatRoute = new Hono()
+export const chatRoute = new Hono().post("/", zValidator("json", chatSchema), getAuthUser, async c => {
+  try {
+    const user = c.get("user")
+    const { id, message, selectedModelId, selectedToolName } = c.req.valid("json")
+    console.log("🚀 ~ message:", message)
+
+    let chat = await prisma.chat.findUnique({
+      where: { id },
+    })
+
+    if (!chat) {
+      const title = await generateTitleForUserMessage({ message })
+      chat = await prisma.chat.create({
+        data: {
+          id,
+          userId: user.id,
+          title,
+        },
+      })
+    }
+
+    const messageFromDB = await prisma.message.findMany({
+      where: { chatId: chat.id },
+      orderBy: { createdAt: "desc" },
+    })
+
+    const mapUIMessages = messageFromDB.map(m => ({
+      id: m.id,
+      role: m.role as "user" | "assistant" | "system",
+      parts: m.parts as UIMessagePart<any, any>[],
+      metadata: {
+        createdAt: m.createdAt,
+      },
+    }))
+
+    // 添加新的消息
+    const newUIMessages = [...mapUIMessages, message]
+
+    // 转换成 model 格式
+    const modelMessages = convertToModelMessages(newUIMessages)
+
+    // 新的消息创建到数据库
+    await prisma.message.create({
+      data: {
+        id: message.id,
+        role: "user",
+        parts: JSON.parse(JSON.stringify(message.parts)),
+        chatId: chat.id,
+      },
+    })
+
+    // const modelProvider = isProduction ? ModelProvider.languageModel(selectedModelId) : ModelProvider.languageModel(DEVELOPMENT_CHAT_MODEL)
+    const modelProvider = ModelProvider.languageModel(DEVELOPMENT_CHAT_MODEL)
+
+    const result = streamText({
+      model: modelProvider,
+      messages: modelMessages,
+      stopWhen: stepCountIs(5),
+      // tools: [],
+      toolChoice: "auto",
+      onError({ error }) {
+        console.error(error)
+      },
+    })
+
+    return result.toUIMessageStreamResponse({
+      sendSources: true,
+      generateMessageId: () => generateUUID(),
+      originalMessages: newUIMessages,
+      onFinish: async ({ messages, responseMessage }) => {
+        console.log("🚀 ~ messages, responseMessage:", messages, responseMessage)
+        try {
+          await prisma.message.createMany({
+            data: messages.map(m => ({
+              id: m.id || generateUUID(),
+              role: m.role,
+              parts: JSON.parse(JSON.stringify(message.parts)),
+              chatId: chat.id,
+              createdAt: new Date(),
+              updatedAt: new Date(),
+            })),
+            skipDuplicates: true, // 避免重复
+          })
+        } catch (error) {
+          console.log("error", error)
+        }
+      },
+    })
+  } catch (error) {
+    if (error instanceof HTTPException) {
+      throw error
+    }
+    throw new HTTPException(500, { message: "Internal server error" })
+  }
+})
---
+++ lib/actions.ts
@@ -0,0 +1,22 @@
+"use server"
+
+import { generateText, type UIMessage } from "ai"
+import { ModelProvider } from "./ai/providers"
+
+export async function generateTitleForUserMessage({ message }: { message: UIMessage }) {
+  try {
+    const { text } = await generateText({
+      model: ModelProvider.languageModel("title-model"),
+      system: `\n
+        - you will generate a short title based on the first message a user begins a conversation with
+        - ensure it is not more than 80 characters long
+        - the title should be a summary of the user's message
+        - do not use quotes or colons`,
+      prompt: JSON.stringify(message),
+    })
+    return text
+  } catch (error) {
+    console.log("title ai error", error)
+    return "Untitled"
+  }
+}
